2aee3be47a97a63e7d6eceeac2dc8a0951be4fb0
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-13 08:06:50 +0300

    add empty devtools page

---
+++ src/devtools/devtools_page.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create(
+  'RxJs DevTools',
+  'src/assets/icons/icon-512x512.png"',
+  'src/devtools/devtools_panel_page.html',
+  function (panel) {
+    // code invoked on panel creation
+  }
+);
