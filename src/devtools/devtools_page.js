ac2341798414cdf59f0e7db4e1b8cfd13d946264
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-13 08:52:17 +0300

    update devtools_page

--- src/devtools/devtools_page.js
+++ src/devtools/devtools_page.js
@@ -1,8 +1,10 @@
+'use strict';
+
 chrome.devtools.panels.create(
   'RxJs DevTools',
   'src/assets/icons/icon-512x512.png"',
   'src/devtools/devtools_panel_page.html',
-  function (panel) {
+  (_panel) => {
     // code invoked on panel creation
   }
 );
