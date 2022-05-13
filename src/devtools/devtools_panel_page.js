cdc1f30f2e8a8ded830f20986067c2abb47a0f58
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-13 18:15:09 +0300

    add stats logic

--- src/devtools/devtools_panel_page.js
+++ src/devtools/devtools_panel_page.js
@@ -5,9 +5,32 @@ const backgroundConnection = chrome.runtime.connect({
 });
 
 backgroundConnection.onMessage.addListener((message) => {
-  console.log('devtools_panel_page', message);
+  if (!message.type || !message.data) {
+    return;
+  }
+
+  const { type, data } = message;
+
+  switch (type) {
+    case 'stats':
+      handleStats(data);
+      break;
+  }
 });
 
 const sendMessage = (message) => {
   backgroundConnection.postMessage(message);
 };
+
+const handleStats = (data) => {
+  document.getElementById('statsNext').textContent = data.next;
+  document.getElementById('statsError').textContent = data.error;
+  document.getElementById('statsComplete').textContent = data.complete;
+  document.getElementById('statsSubscribe').textContent = data.subscribe;
+  document.getElementById('statsUnsubscribe').textContent = data.unsubscribe;
+};
+
+sendMessage({
+  type: 'init',
+  data: {},
+});
